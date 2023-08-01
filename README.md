41beb455704876449c904681e180811a1e85ce96
Michie <contact@michiewillman.com>
2023-08-01 15:57:24 -0700

    Routes updated

---
+++ README.md
@@ -0,0 +1,87 @@
+# E-commerce Back-End Application
+
+## Description
+
+This application is a working Express.js API that utilizes Sequelize to interact with a MySQL database.
+
+E-commerce plays a significant role within the electronics industry, as it empowers businesses and consumers alike to conveniently engage in online buying and selling of electronic products.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Deployment](#deployment)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+To view this application's repository code and try it for yourself, [click here.](https://github.com/michiewillman/ecommerce-back-end-db)
+
+You can clone this repo from GitHub via SSH using the following command to use this application on your local machine.
+
+```bash
+git clone git@github.com:michiewillman/ecommerce-back-end-db.git
+```
+
+Install the dependencies:
+
+```bash
+npm install
+```
+
+Run MySQL in terminal, then enter your password.
+
+```bash
+mysql -u root -p
+```
+
+Source database schema file:
+
+```bash
+source <path to schema.sql>
+```
+
+Source database seeds:
+
+```bash
+source <>
+```
+
+Close the MySQL terminal and run the application in node:
+
+```bash
+node index.js
+```
+
+## Usage
+
+For a video showing the application's functionality, [click here.]()
+
+# Deployment
+
+N/A
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+MIT
+
+For more information on this license, [click here.](https://opensource.org/license/https://opensource.org/licenses/MIT)
+
+## Contributing
+
+N/A
+
+## Tests
+
+N/A
+
+## Questions
+
+If you have any questions, please open an issue or contact me via email at [contact@michiewillman.com](mailto:contact@michiewillman.com).
+
+You can view more of my work on GitHub [here](https://github.com/michiewillman).
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,82 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
+const { findByPk, findAll } = require("../../models/Product");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get("/", async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: Product,
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: "No categories found." });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get("/:id", async (req, res) => {
+  try {
+    const idCategory = await Category.findByPk(req.params.id, {
+      include: Product,
+    });
+
+    if (!idCategory) {
+      res.status(404).json({ message: "No category found with that id." });
+      return;
+    }
+
+    res.status(200).json(idCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post("/", async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: Product,
+    });
+
+    // create a new category
+
+    res.status(200).json(`Category has been added.`);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: Product,
+    });
+    // add logic to update category
+
+    res.status(200).json(`Category has been updated`);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: Product,
+    });
+
+    // delete a category by its `id` value
+
+    res.status(200).json(`Category has been deleted.`);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
