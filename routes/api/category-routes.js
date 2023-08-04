b975ce6e23afa9dd1fd6f4aa1fb847cecaa1164e
Michie <contact@michiewillman.com>
2023-08-03 22:47:58 -0700

    Category PUT is working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,9 +43,7 @@ router.get("/:id", async (req, res) => {
 // Create category in database
 router.post("/", async (req, res) => {
   try {
-    const categoryData = await Category.create({
-      category_name: req.body.category_name,
-    });
+    const categoryData = await Category.create(req.body);
 
     res
       .status(200)
@@ -68,8 +66,6 @@ router.put("/:id", async (req, res) => {
       res.status(404).json({ message: "No category found with that id." });
       return;
     }
-
-    await categoryData.save();
 
     res.status(200).json(`Category has been updated.`);
   } catch (err) {
