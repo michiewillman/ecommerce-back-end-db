fe233d1c31bf594d5bf49b2c596f568dc53bca6c
Michie <contact@michiewillman.com>
2023-08-03 18:07:53 -0700

    Update 'through' on product & tag model index

--- models/index.js
+++ models/index.js
@@ -17,12 +17,18 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
 });
 
 module.exports = {
